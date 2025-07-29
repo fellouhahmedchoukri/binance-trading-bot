cb12d1b5da3977d3c8e3dec0d62c77b4513441fc
FAC <57494827+fellouhahmedchoukri@users.noreply.github.com>
2025-07-29 23:15:14 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,26 +1,26 @@
-from app import db
 from datetime import datetime
+from main import db
 
-class TradingSnapshot(db.Model):
+class Snapshot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    equity = db.Column(db.Float)
-    net_profit = db.Column(db.Float)
-    open_positions = db.Column(db.Integer)
-    pending_orders = db.Column(db.Integer)
-    btc_price = db.Column(db.Float)
-    
-    def __repr__(self):
-        return f'<Snapshot {self.timestamp}>'
+    total_balance = db.Column(db.Float)
+    available_balance = db.Column(db.Float)
+    positions_count = db.Column(db.Integer)
 
-class TradeHistory(db.Model):
+class Trade(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    symbol = db.Column(db.String(10))
+    symbol = db.Column(db.String(20))
     side = db.Column(db.String(10))
     quantity = db.Column(db.Float)
     price = db.Column(db.Float)
     status = db.Column(db.String(20))
-    
-    def __repr__(self):
-        return f'<Trade {self.symbol} {self.side} {self.quantity}>'
+
+class Position(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    symbol = db.Column(db.String(20))
+    entry_price = db.Column(db.Float)
+    quantity = db.Column(db.Float)
+    side = db.Column(db.String(10))
+    status = db.Column(db.String(20), default='OPEN')
